8c8ce784e1e2866fcfafff64b382c77658bdd72f
Joel Wilkins <joelnwilkins@gmail.com>
2018-01-21 18:23:22 +0000

    Added comments to gearModel.py

--- gearGenerator.py
+++ gearGenerator.py
@@ -1,9 +1,19 @@
+# Import required modules
+
+# matplotlib is the module to generate the graphs
 import matplotlib.pyplot as plt
+# math is the module to use trigonometric functions etc.
 import math
+# pickle is used to save the data to a file
+# I may try to change this to a csv file which would be better
 import pickle
-#import csv
+import random
 
 def frange(start, end, step):
+    # A function to return a list of float values
+    # between 2 points with a certain step
+
+    # If start is smaller than end the values will be ascending
     if start < end:
         values = []
         total = start
@@ -12,6 +22,8 @@ def frange(start, end, step):
             total += step
         values.append(end)
         return values
+
+    # If start is greater than end the values will be descending
     elif start > end:
         values = []
         total = start
@@ -28,6 +40,7 @@ def getAlpha(r, rb):
     return math.acos(rb / r)
 
 def getR(rb, alpha):
+    # This is a rearranged version of getAlpha
     return rb / math.cos(alpha)
 
 def pythagoras(point1, point2):
@@ -37,84 +50,13 @@ def getDeltaTheta(r, s):
     return math.acos((2 * r**2 - s**2) / (2 * r**2))
 
 def cartesian(r, theta):
+    # Converts polar coordinates to cartesian coordinates
     x = r * math.cos(theta)
     y = r * math.sin(theta)
     return (x, y)
 
-def points(r, alpha):
-    if alpha > 0:
-        x, y = cartesian(r, involute(alpha))
-    else:
-        x, y = cartesian(r, -involute(abs(alpha)))
-    return (x, y)
-
-def getPoints(rb, theta):
-    a = rb * math.cos(theta)
-    b = rb * math.sin(theta)
-    x = a + math.sin(theta) * theta * rb
-    y = b - math.cos(theta) * theta * rb
-    return (x, y)
-
-def rotate(point, theta, centre=(0, 0)):
-    x = (point[0] - centre[0]) * math.cos(theta) - (point[1] - centre[1]) * math.sin(theta)
-    y = (point[0] - centre[0]) * math.sin(theta) + (point[1] - centre[1]) * math.cos(theta)
-    return (x, y)
-
-def gearPoints(rb, R, n, gapRatio1, step):
-    angle = 2 * math.pi / n
-    gap1 = gapRatio1 * angle
-    gap2 = angle - (2 * involute(getAlpha(R, rb))) - gap1
-    print(gap1, gap2)
-
-    x = []
-    y = []
-    
-    for i in range(n):
-        #Generate the leading curve
-        for r in frange(rb, R, step):
-            alpha = getAlpha(r, rb)
-            point = points(r, alpha)
-            point = rotate(point, (angle * i) + gap1)
-            x.append(point[0])
-            y.append(point[1])
-        """
-        #Generate the curve on top of the tooth
-        for theta in frange((angle * i) + gap1 + involute(getAlpha(R, rb)), (angle * i) + involute(getAlpha(R, rb)) + gap2, getDeltaTheta(R, step)):
-            point = cartesian(R, theta)
-            x.append(point[0])
-            y.append(point[1])"""
-        
-        #Generate the trailing curve
-        for r in frange(R, rb, step):
-            alpha = getAlpha(r, rb)
-            point = points(r, -alpha)
-            point = rotate(point, (angle * (i + 1)) - gap1)
-            x.append(point[0])
-            y.append(point[1])
-        """
-        #Generate the curve between the teeth
-        for theta in frange((angle * (i + 1)) - gap1, (angle * (i + 1)) + gap1, getDeltaTheta(rb, step)):
-            point = cartesian(rb, theta)
-            x.append(point[0])
-            y.append(point[1])"""
-
-    x.append(x[0])
-    y.append(y[0])
-
-    return (x, y)
-
-def circlePoints(r, step):
-    x = []
-    y = []
-
-    for theta in frange(0, 2 * math.pi, getDeltaTheta(r, step)):
-        point = cartesian(r, theta)
-        x.append(point[0])
-        y.append(point[1])
-
-    return (x, y)
-
 def convertPolar(x, y):
+    # Converts a list of x and y values to polar form
     a = []
     b = []
     for i in range(len(x)):
@@ -122,30 +64,114 @@ def convertPolar(x, y):
         b.append(math.atan(y[i] / x[i]))
     return (a, b)
 
-def rotatePoints(points, theta, centre=(0, 0)):
+def points(r, alpha):
+    # This generates the curved part of the tooth
+    if alpha > 0:
+        x, y = cartesian(r, involute(alpha))
+    else:
+        x, y = cartesian(r, -involute(abs(alpha)))
+    return (x, y)
+
+def getPoints(rb, theta):
+    # This is a cartesian version of points and is less good
+    a = rb * math.cos(theta)
+    b = rb * math.sin(theta)
+    x = a + math.sin(theta) * theta * rb
+    y = b - math.cos(theta) * theta * rb
+    return (x, y)
+
+def rotate(point, theta, centre):
+    # This can be used to rotate a point about a centre
+    x = (point[0] - centre[0]) * math.cos(theta) - (point[1] - centre[1]) * math.sin(theta)
+    y = (point[0] - centre[0]) * math.sin(theta) + (point[1] - centre[1]) * math.cos(theta)
+    return (x, y)
+
+def rotatePoints(points, theta, centre):
+    # This implements the rotate function for a list of points
     a = []
     b = []
     for p in points:
-        point = rotate(p, theta, centre=centre)
+        point = rotate(p, theta, centre)
         a.append(point[0])
         b.append(point[1])
     return list(zip(a, b))
 
-def rotatePointList(x, y, theta, centre=(0, 0)):
+def rotatePointList(x, y, theta, centre):
+    # This implements the rotate function on a list of x and y values
     a = []
     b = []
     for i in range(len(x)):
-        point = rotate((x[i], y[i]), theta, centre=centre)
+        point = rotate((x[i], y[i]), theta, centre)
         a.append(point[0])
         b.append(point[1])
     return (a, b)
 
+def gearPoints(rb, R, n, gapRatio1, step):
+    # This is the main function to generate a gear
+    # The 2nd and 4th sections may have to be commented out
+    # if the gear is being used in the 3D model
+    # Including them can cause the model to not run
+    
+    angle = 2 * math.pi / n
+    # gap1 is the angle of the gap between the teeth
+    gap1 = gapRatio1 * angle
+    # gap2 is the angle of the top of the tooth
+    gap2 = angle - (2 * involute(getAlpha(R, rb))) - gap1
+
+    x = []
+    y = []
+    for i in range(n):
+        # Generate the leading curve
+        for r in frange(rb, R, step):
+            alpha = getAlpha(r, rb)
+            point = points(r, alpha)
+            point = rotate(point, (angle * i) + gap1, (0, 0))
+            x.append(point[0])
+            y.append(point[1])
+        """
+        # Generate the curve on top of the tooth
+        for theta in frange((angle * i) + gap1 + involute(getAlpha(R, rb)), (angle * i) + involute(getAlpha(R, rb)) + gap2, getDeltaTheta(R, step)):
+            point = cartesian(R, theta)
+            x.append(point[0])
+            y.append(point[1])"""
+        
+        # Generate the trailing curve
+        for r in frange(R, rb, step):
+            alpha = getAlpha(r, rb)
+            point = points(r, -alpha)
+            point = rotate(point, (angle * (i + 1)) - gap1, (0, 0))
+            x.append(point[0])
+            y.append(point[1])
+        """
+        # Generate the curve between the teeth
+        for theta in frange((angle * (i + 1)) - gap1, (angle * (i + 1)) + gap1, getDeltaTheta(rb, step)):
+            point = cartesian(rb, theta)
+            x.append(point[0])
+            y.append(point[1])"""
+
+    # Add the first values to the end to make the gear meet up
+    x.append(x[0])
+    y.append(y[0])
+    return (x, y)
+
+def circlePoints(r, step):
+    # This function will return a list of points for a circle of radius r
+    x = []
+    y = []
+    for theta in frange(0, 2 * math.pi, getDeltaTheta(r, step)):
+        point = cartesian(r, theta)
+        x.append(point[0])
+        y.append(point[1])
+    return (x, y)
+
 def inside(point, centre, a, b):
+    # This function works out if a point is inside a gear
     d = pythagoras(point, centre)
     try:
         theta = math.atan((point[1] - centre[1]) / (point[0] - centre[0]))
     except ZeroDivisionError:
-        theta = math.atan((point[1] - centre[1]) / 0.0001)
+        # An exception for if the point has the same x value as the centre
+        theta = math.pi / 2
     diff = []
     for item in b:
         diff.append(abs(item - theta))
@@ -158,14 +184,15 @@ def inside(point, centre, a, b):
         return False
 
 def intersecting(gearPoints1, centre1, angle1, gearPoints2, centre2, angle2):
+    # This function tries to determine if 2 gears intersect or are touching
     x = []
     y = []
     for point in gearPoints1:
         x.append(point[0])
         y.append(point[1])
         
-    a, b = rotatePointList(x, y, angle1, centre=centre1)
-    gearPoints2 = rotatePoints(gearPoints2, angle2, centre=centre2)
+    a, b = rotatePointList(x, y, angle1, centre1)
+    gearPoints2 = rotatePoints(gearPoints2, angle2, centre2)
     for point in gearPoints2:
         var = inside(point, centre1, a, b)
         if var == True:
@@ -177,6 +204,8 @@ def intersecting(gearPoints1, centre1, angle1, gearPoints2, centre2, angle2):
     plt.show()
     #return False
 
+# If this program is being run directly this code will be executed
+# If this program is being imported this code will not be executed
 if __name__ == "__main__":
     lines = []
 
@@ -189,17 +218,16 @@ if __name__ == "__main__":
 
     x, y = gearPoints(rb, R, n, gapRatio1, step)
 
+    # This adds a dictionary of the gear parameters
     data = [{"rb": rb, "R": R, "n": n, "angle": angle}]
-    data = []
+    # This adds the list of x and y values in coordinate form
     data.extend(list(zip(x, y)))
-    """
-    for i in range(len(x)):
-        data.append([x[i], y[i]])
-    """
 
-    with open("gearData2.pkl", "wb") as f:
+    # This writes the data to a file to be read by the gearModel programs
+    with open("gearData.pkl", "wb") as f:
         pickle.dump(data, f)
 
+    # A test of how write to a csv file, not currently working
     """
     with open("gearData2.csv", "wb") as f:
         csvout = csv.writer(f, delimiter=' ',
@@ -210,6 +238,8 @@ if __name__ == "__main__":
             csvout.writerow([item[0], item[1]])
     """
 
+    # This will draw 2 circles with radius rb and R
+    # Uncomment this if you want to see how the gear lies on these circles
     """
     lines.append(circlePoints(rb, step))
     lines.append(circlePoints(R, step))
@@ -217,24 +247,28 @@ if __name__ == "__main__":
     for line in lines:
         plt.plot(line[0], line[1], color="blue")
     """
-
-    print(len(x))
-
+    
+    # Generate some random points and test if they are inside the gear
+    # This is to test the inside function
     a, b = convertPolar(x, y)
-    pointsToCheck = [(1, 0)]
-    import random
-    for i in range(100):
+    pointsToCheck = [(1, 0), (0, 1)]
+    for i in range(1):
         pointsToCheck.append((random.uniform(-2, 2), random.uniform(-2, 2)))
 
     for p in pointsToCheck:
         var = inside(p, (0, 0), a, b)
         if var == True:
+            # The point will be green if it is inside
             plt.plot(p[0], p[1], "go")
         if var == None:
+            # The point will be yellow if it is just touching the gear
             plt.plot(p[0], p[1], "yo")
         if var == False:
+            # The point will be red if it is outside the gear
             plt.plot(p[0], p[1], "ro")
 
+    # This will draw the gear on a graph
     plt.plot(x, y, color="red")
+    # The equal means that the graph will not be distorted
     plt.axis("equal")
     plt.show()
--- gearModel.py
+++ gearModel.py
@@ -1,16 +1,28 @@
+# Import required modules
+
+# vpython is the module for 3D graphics
 from vpython import *
+# math is the module to use trigonometric functions etc.
 import math
+# pickle is used to save the data to a file
+# I may try to change this to a csv file which would be better
 import pickle
+# This imports functions from the gear generating program
 from gearGenerator import *
 
 class SpeedControl(slider):
+    # A class to control the slider which changes the gear speed
+    
     def __init__(self, *args, **kwargs):
+        # Initiate the slider object
         super().__init__(bind=self.updateSpeed, *args, **kwargs)
 
     def updateSpeed(self):
+        # Does nothing but required as the slider needs a binding
         pass
 
     def getSpeed(self):
+        # Returns the value of the slider
         return self.value
 
 def setCaption(text):
@@ -19,19 +31,26 @@ def setCaption(text):
 def textOut(text):
     scene.append_to_caption("\n" + text)
 
+# Create a new scene and select it to be shown
 scene = canvas(title="Gear simulation")
 scene.select()
 
+# A list of the items in the caption
 captionText = []
 
-with open("gearData2.pkl", "rb") as f:
+# Load the data from the file
+with open("gearData.pkl", "rb") as f:
     data = pickle.load(f)
 
+# Get the gear parameters and remove them from the data
 parameters = data[0]
 data = data[1:]
 
+# Create a 2D profile of the gear
 gearProfile = shapes.points(pos=data)
 
+# extrusion is used to project a 2D shape to 3D
+# Rotate the gears so that they face the camera
 setCaption("Building gear 1...")
 gear1 = extrusion(path=[vector(-0.05, 0, 0), vector(0.05, 0, 0)], shape=gearProfile, axis=vector(0, 0, 0))
 gear1.pos = vector(-1.1, 0, 0)
@@ -44,11 +63,14 @@ gear2.rotate(angle=math.pi/2, axis=vector(0, 1, 0), origin=gear2.pos)
 gear2.rotate(angle=math.pi, axis=vector(1, 0, 0), origin=gear2.pos)
 gear2.rotate(angle=parameters["angle"]/2, axis=gear2.axis, origin=gear2.pos)
 
+# Clear the caption and create the speed control slider
 setCaption("")
 s = SpeedControl(min=0, max=0.1, value=0.01)
 
 while True:
+    # Rotate the gears depending on the speed
     gear1.rotate(angle=s.getSpeed(), axis=gear1.axis, origin=gear1.pos)
     gear2.rotate(angle=s.getSpeed(), axis=gear2.axis, origin=gear2.pos)
     
+    # Slow down the program to avoid using too much CPU power
     rate(25)
